d0362e1b1b138d80c1819a662234ddea378a9332
Liooouu <apiadojohnliol@gmail.com>
2025-07-13 01:12:21 +0800

    fixed some major problems

--- portfolio_proj/src/components/Navbar.jsx
+++ portfolio_proj/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import {cn} from "@/lib/utils";
 import { useEffect, useState } from "react";
+import { Menu, X } from 'lucide-react';
 
 
 const navItems = [
@@ -27,9 +28,9 @@ export const Navbar = () => {
     
     return(
     <nav className={cn("fixed w-full z-40 transition-all duration-300", 
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        isScrolled ? "py-1 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
     )}>
-        <div className="contain flex items-center justify-between">
+        <div className="container flex items-center justify-between">
             <a 
             className="text-xl font-bold text-primary justify-between" 
             href="#hero"
@@ -39,16 +40,26 @@ export const Navbar = () => {
                 </span>
             </a>
 
-            
             <div className="hidden md:flex space-x-8">
                 {navItems.map((item, key) => (
                     
-                    <a key={key}href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300"> 
-                        {item.name}
-                        </a>
+                    <a 
+                    
+                    key={key}
+                    href={item.href} 
+                    className="text-foreground/80 hover:text-primary transition-colors duration-300"> 
+                    {item.name}
+
+                    </a>
 
                 ))}
             </div>
+
+            <button onClick={() => setIsMenuOpen((prev) => !prev)} 
+            className="md:hidden p-2 text-foreground z-50">
+                {isMenuOpen ? <X size={24}/> : <Menu size={24} />}
+                <aria-label>{isMenuOpen ? "Close Menu" : "Open Menu"}</aria-label>
+            </button>
             
             <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
                 "transition-all duration-300 md:hidden",
@@ -67,13 +78,11 @@ export const Navbar = () => {
                     > 
                         {item.name}
                     </a>
-
+                    
                 ))}
-            </div>
-
-            </div>
-            
-        </div>
+             </div>
+          </div>
+       </div>
     </nav>
-    );
+  );
 }
--- portfolio_proj/src/components/StarBackgrounds.jsx
+++ portfolio_proj/src/components/StarBackgrounds.jsx
@@ -5,24 +5,21 @@ export const StarBackground = () => {
     const [meteors, setMeteors] = useState([]);
 
     useEffect(() => {
+
         generateStars();
         generateMeteors();
-
-
         const handleResize = () => {
             generateStars();
         };
 
         window.addEventListener('resize', handleResize)
-
         return () => window.removeEventListener("resize", handleResize);
-
 
     }, []);
 
     const generateStars =() => {
         const numberOfStars = Math.floor(
-            (window.innerWidth * window.innerHeight) / 1000
+            (window.innerWidth * window.innerHeight) / 10000
      ); 
 
      const newStars = []
@@ -38,23 +35,20 @@ export const StarBackground = () => {
         });
      }
 
-     setStars(newStars);
+    setStars(newStars);
 
     };
 
-
     const generateMeteors =() => {
-        const numberOfMeteors = 3 
+        const numberOfMeteors = 4 
         const newMeteors = [];
-
-     const newStars = []
 
      for (let i=0; i < numberOfMeteors; i++) {
         newMeteors.push({
             id:i,
             size: Math.random() * 2 + 1,
             x: Math.random() * 100,
-            y: Math.random() * 20,
+            y: Math.random() * 60,
             delay: Math.random() * 15,
             animationDuration: Math.random() * 3 + 3,
         });
@@ -88,8 +82,6 @@ export const StarBackground = () => {
                 animationDuration: meteor.animationDuration + "s",
             }}/>
         ))}
-
     </div> 
-
-    );
+  );
 }
